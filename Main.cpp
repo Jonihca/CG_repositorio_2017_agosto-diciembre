8fd0ba78a23ca3de0116980fcfa34b163de1224a
Isaac Hinojosa Padilla <jonihca.hp@gmail.com>
2017-10-09 14:56:02 -0500

    02-10-17  TAREA_mesh

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,110 @@
-/***********************************************
-Materia:	Gráficas Computacionales
-Fecha:		21 de Agosto del 2017
-Autor:		A01375843 Isaac Hinojosa Padilla
-************************************************/
+#include <iostream>
+#include <GL/glew.h>
+#include <vector>
+#include <glm/glm.hpp>
+#include <GL/freeglut.h>
 
-/*#include <iostream>
+void Initialize()
+{
+	// Creando toda la memoria que el programa va a utilizar.
 
-#include "InputFile.h"
+	// Creación del atributo de posiciones de los vértices.
+	// Lista de vec2
+	// Claramente en el CPU y RAM
+	std::vector<glm::vec2> positions;
+	positions.push_back(glm::vec2(0.5f, -0.5f));
+	positions.push_back(glm::vec2(0.5f, 0.5f));
+	positions.push_back(glm::vec2(-0.5f, -0.5f));
+	positions.push_back(glm::vec2(-0.5f, 0.5f));
+	// Arreglo de colores en el cpu
+	std::vector<glm::vec3> colors;
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+}
+
+void GameLoop()
+{
+	// Limpiamos el buffer de color y el buffer de profunidad.
+	// Siempre hacerlo al inicio del frame
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Cuando terminamos de renderear, cambiamos los buffers.
+	glutSwapBuffers();
+}
+void Idle()
+{
+	// Cuando OpenGL entra en modo de reposo 
+	// (para guardar bateria, por ejemplo)
+	// le decimos que vuelva a dibujar ->
+	// Vuelve a mandar a llamar GameLoop
+	glutPostRedisplay();
+}
+
+void ReshapeWindow(int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 
 int main(int argc, char* argv[])
 {
-	std::string filename = "Prueba.txt";   
-	InputFile myFile;   
-	myFile.Read(filename);   
-	std::string contents = myFile.GetContents();
-	std::cout << "Contents: " << contents << std::endl;
+	// Inicializar freeglut
+	// Freeglut se encarga de crear una ventana
+	// en donde podemos dibujar
+	glutInit(&argc, argv);
+	// Solicitando una versión específica de OpenGL.
+	glutInitContextVersion(4, 4);
+	// Iniciar el contexto de OpenGL. El contexto se refiere
+	// a las capacidades que va a tener nuestra aplicación
+	// gráfica.
+	// En este caso estamos trabajando con el pipeline programable.
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	// Freeglut nos permite configurar eventos que ocurren en la ventana.
+	// Un evento que nos interesa es cuando alguien cierra la ventana.
+	// En este caso, simplemente dejamos de renderear la esscena y terminamos el programa.
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	// Configuramos el framebuffer. En este caso estamos solicitando un buffer
+	// true color RGBA, un buffer de profundidad y un segundo buffer para renderear.
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	// Iniciar las dimensiones de la ventana (en pixeles)
+	glutInitWindowSize(400, 400);
+	// Creamos la ventana y le damos un título.
+	glutCreateWindow("Hello World GL");
+	// Asociamos una función de render.
+	//Esta función se mandará a llamar para dibujar un frame.
+	glutDisplayFunc(GameLoop);
+	// Asociamos una función para el cambio de resolución
+	// de la ventana. Freeglut la va a mandar a llamar
+	// cuando alguien cambie el tamaño de la venta.
+	glutReshapeFunc(ReshapeWindow);
+	// Asociamos la función que se mandará a llamar
+	// cuando OpenGL entre en modo de reposo.
+	glutIdleFunc(Idle);
 
-	std::cin.get();
+	// Inicializar GLEW. Esta librería se encarga de obtener el API de OpenGL de
+	// nuestra tarjeta de video. SHAME ON YOU MICROSOFT.
+	glewInit();
+
+	// Configurar OpenGL. Este es el color por default del buffer de color
+	// en el framebuffer.
+	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+	// Ademas de solicitar el buffer de profundidad, tenemos
+	// que decirle a OpenGL que lo queremos activo
+	glEnable(GL_DEPTH_TEST);
+	// Activamos el borrado de caras traseras.
+	// Ahora todos los triangulos que dibujemos deben estar en CCW
+	glEnable(GL_CULL_FACE);
+	// No dibujar las caras traseras de las geometrías.
+	glCullFace(GL_BACK);
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	// Configuración inicial de nuestro programa.
+	Initialize();
+
+	// Iniciar la aplicación. El main se pausará en esta línea hasta que se cierre
+	// la venta de OpenGL.
+	glutMainLoop();
 
 	return 0;
-}*/
+}
